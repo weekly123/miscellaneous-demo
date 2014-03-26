7ced853d0630fbca2ff3170eaca09fffc33ab74d
weekly123 <weekly123@gmail.com>
2014-03-26 23:12:05 +0800

    first comit

--- miscellaneous/src/main/java/miscellaneous/example/JavaUtilLoggingExample.java
+++ miscellaneous/src/main/java/miscellaneous/example/JavaUtilLoggingExample.java
@@ -1,9 +1,20 @@
 package miscellaneous.example;
 
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.SocketHandler;
+
+
+/**
+ * This is a example about how to using java.util.logging.
+ * @author weekly123@gmail.com	
+ *
+ */
+
 public class JavaUtilLoggingExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
